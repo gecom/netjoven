81975182a1b43a222807cff4cd4bf6edacbd0bb7
Pavel <neo2318@gmail.com>
2014-04-29 08:56:43 -0500

    importar

--- docs/scripts/imporat.sql
+++ docs/scripts/imporat.sql
@@ -1,3 +1,3 @@
 mysql -h localhost -u root -p --default_character_set utf8 -f netjoven < D:\xampp\htdocs\netjoven-v2\docs\scripts\netjoven_27_04_14_1152.sql
 
-mysqldump -h localhost -u root -p netjoven_local > fichero.sql
+mysqldump -h localhost -u root -p netjoven_local > netjoven_28_04_14.sql
