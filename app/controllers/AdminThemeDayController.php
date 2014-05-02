f6eb04882de07fb7bbee25b151d5a411b7fafed5
Pavel <neo2318@gmail.com>
2014-05-02 14:30:36 -0500

    cambios

--- app/controllers/AdminThemeDayController.php
+++ app/controllers/AdminThemeDayController.php
@@ -30,8 +30,7 @@ class AdminThemeDayController extends BaseController {
         $dbr_theme_day = ($is_new == true ? new ThemeDay() : $theme_day);
 
         $rules = array(
-            'name'   => 'required|min:3',
-            'category_id'      => 'required|numeric'
+            'name'   => 'required|min:3'
         );
 
         $validator = Validator::make($data_frm_theme_day, $rules);
@@ -45,17 +44,58 @@ class AdminThemeDayController extends BaseController {
             }
         }else{
 
+
+        		/*$data_sections = ThemeDay::find(1)->sections()->get();
+
+
+        		print_r($data_sections);
+
+        		die();*/
+
+
+        		try {
+					$tag_data_id = Helpers::getTagIds(array($data_frm_theme_day['name']));
+
+					if(count($tag_data_id) == 0){
+
+					}
+
+					$dbr_theme_day->tag_id = $tag_data_id[0];
+
+					if(!empty($data_frm_theme_day['params'])){
+						$dbr_theme_day->params = $data_frm_theme_day['params'];
+					}
+
+					if(!empty($data_frm_theme_day['color'])){
+						$dbr_theme_day->color = $data_frm_theme_day['color'];
+					}
+
+					if($dbr_theme_day->save()){
+						$dbr_theme_day->sections = $data_frm_theme_day['sections'];
+						$response['success'] = true;
+						$response['message'] =  'Nota registrada satisfactorimente';
+						$response['redirect'] =  route('backend.theme_day.list');
+					}else{
+						$response['success'] = false;
+						$response['errors'] =  ['Error: Hubo un error al registrar el tema del dia'];
+					}
+
+        		} catch (Exception $e) {
+					$response['success'] = false;
+					$response['errors'] =  ['Error:' . $e->getMessage()];
+        		}
+
+
+
+
+
+/*print_r($name);
+
+die();
 			$dbr_theme_day->name = $data_frm_theme_day['name'];
-			$dbr_theme_day->slug = $data_frm_theme_day['name'];
 			$dbr_theme_day->category_id = $data_frm_theme_day['category_id'];
 
-			if(!empty($data_frm_theme_day['params'])){
-				$dbr_theme_day->params = $data_frm_theme_day['params'];
-			}
 
-			if(!empty($data_frm_theme_day['color'])){
-				$dbr_theme_day->params = $data_frm_theme_day['params'];
-			}
 
 			try {
 				if($dbr_theme_day->save()){
@@ -69,7 +109,7 @@ class AdminThemeDayController extends BaseController {
 			} catch (Exception $e) {
 				$response['success'] = false;
 				$response['errors'] =  ['Error:' . $e->getMessage()];
-			}
+			}*/
 
         }
 
--- app/models/ThemeDay.php
+++ app/models/ThemeDay.php
@@ -10,6 +10,16 @@ class ThemeDay extends Eloquent {
 		return $this->belongsTo('Category');
 	}
 
+	public function sections()
+    {
+        return $this->belongsToMany('Category', 'njv_theme_day_section');
+    }
+
+    public function setSectionsAttribute($category_ids)
+    {
+        $this->sections()->sync($category_ids, true);
+    }
+
 	public function setSlugAttribute($value){
 
 		$slug = Str::slug($value);
@@ -24,9 +34,24 @@ class ThemeDay extends Eloquent {
 	public static function getThemeDay(){
 
 		$dbl_theme_day = (new ThemeDay())
-		->select('id', 'name', DB::raw('(SELECT slug from njv_category c where c.id = njv_theme_day.category_id ) as category_slug'),'slug', 'params', 'color');
+			->select('njv_theme_day.tag_id', 'njv_tag.tag', 'njv_tag, slug', 'njv_theme_day.color', 'njv_theme_day.params', DB::raw('GROUP_CONCAT(s.category_id) as sections'))
+			->join('njv_tag', 'njv_tag.id', '=', 'njv_theme_day.tag_id')
+			->leftJoin('njv_theme_day_section', 'njv_theme_day.id', '=', 'njv_theme_day_section.theme_day_id')
+			->groupBy('njv_theme_day.tag_id');
 
 		return $dbl_theme_day;
+
+
+
+		/*select a.tag_id, a.color, params, GROUP_CONCAT(s.category_id) as sections from njv_theme_day a
+INNER JOIN njv_tag b ON a.tag_id = b.id
+LEFT JOIN njv_theme_day_section s ON a.id = s.theme_day_id
+GROUP BY a.tag_id
+
+		$dbl_theme_day = (new ThemeDay())
+		->select('id', 'name', DB::raw('(SELECT slug from njv_category c where c.id = njv_theme_day.category_id ) as category_slug'),'slug', 'params', 'coloz<r');
+
+		return $dbl_theme_day;*/
 
 //		SELECT name,(SELECT slug from njv_category c where c.id = d.category_id ) as category_slug, slug, params, color from njv_theme_day d
 	}
