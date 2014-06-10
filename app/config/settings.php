746b384c4f8a1cd07ddb526e6870ba1884b09e73
Pavel <neo2318@gmail.com>
2014-06-10 17:10:46 -0500

    cambios

--- app/config/settings.php
+++ app/config/settings.php
@@ -1,10 +1,17 @@
 <?php 
 
 return array(
+		'base_url' 	=> 'http://prueba.netjoven.pe/public/',
+		'upload'	=> '/home/static/photos-a.netjoven.pe/httpdocs/_files/',
+		'urlupload'	=> 'http://photos-a.netjoven.pe/_files',
+		'dir_user'	=> ''
+);
+
+/*return array(
 		'base_url' 	=> 'http://netjovenv2.local',
 		'upload'	=> 'D:\/xampp\/htdocs\/photos_netjoven\/_files\/',
 		'urlupload'	=> 'http://photos-a.netjovenv2.local/_files/',
 		'dir_user'	=> ''
-	);
+	);*/
 
 ?>
